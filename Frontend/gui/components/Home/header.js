f688c48fc8ee23745e66fae7cef203e7506c3052
Esther Ekwonye <estherekwonye9@gmail.com>
2022-07-29 16:38:48 +0100

    landing page header and section

--- Frontend/gui/components/Home/header.js
+++ Frontend/gui/components/Home/header.js
@@ -4,5 +4,82 @@ import styles from "../../styles/Home/header.module.css";
 import images from "../../assets/images/images";
 
 export default function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <h2 className={styles.logo}>CompareX</h2>
+        <ul className={styles.list}>
+          <li className={styles.list__item}>
+            <a href="#work">How it works?</a>
+          </li>
+          <li className={styles.list__item}>
+            <a href="#client">Our Clients</a>
+          </li>
+          <li className={styles.list__item}>
+            <a href="#compare">Reviews</a>
+          </li>
+        </ul>
+        <span>
+          <a href="#" className={styles.btn_1}>
+            Sign In
+          </a>
+          <a href="#" className={styles.btn_2}>
+            Sign Up for free
+          </a>
+        </span>
+      </nav>
+
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <h4 className={styles.tool}>
+            <span className={styles.crown}>
+              <Image src={images.crown} />
+            </span>{" "}
+            NEW TOOL
+          </h4>
+          <h1 className={styles.easier}>
+            Comparing Excel Sheets
+            <br />
+            Just Got Easier
+          </h1>
+          <p className={styles.save}>
+            Save your time and build and build productivity with the world-best
+            performing excel
+            <br />
+            comparison tool
+          </p>
+          <span className={styles.use}>
+            <a href="#" className={styles.btn_3}>
+              Use for free
+            </a>
+            <div className={styles.watch__demo}>
+              <a href="#view" className={styles.btn_4}>
+                Watch Demo
+              </a>
+              <div className={styles.arrow}>
+                <Image src={images.vector} />
+              </div>
+            </div>
+          </span>
+        </div>
+        {/* End of Background */}
+        {/* Video */}
+        <video
+          className={styles.video}
+          width="700"
+          height="400"
+          id="view"
+          controls
+          muted
+        >
+          <source src="Images/DEMO VIDEO.svg" type="video/mp4" />
+          <source
+            src="Images/DEMO VIDEO.svg"
+            type="video/ogg"
+            alt="Demo Video"
+          />
+        </video>
+      </main>
+    </header>
+  );
 }
--- Frontend/gui/components/Home/section.js
+++ Frontend/gui/components/Home/section.js
@@ -4,5 +4,31 @@ import styles from "../../styles/Home/section.module.css";
 import images from "../../assets/images/images";
 
 export default function Section() {
-  return <section className={styles.section}></section>;
+  return (
+    <section className={styles.section}>
+      <div className={styles.used_by} id="client">
+        <Image src={images.lovedBy} alt="loved by" />
+      </div>
+      <div className={styles.icons}>
+        <Image src={images.company} alt="Client Companies" />
+      </div>
+
+      <h2 className={styles.how} id="work">
+        How does
+        <span className={styles.how_1}>
+          {"  "} CompareX{"  "}{" "}
+        </span>
+        work?
+      </h2>
+      <span>
+        <p className={styles.steps}>
+          Compare the differences between the Two(2) Excel <br />
+          spreedsheets in 3 EASY STEPS
+        </p>
+      </span>
+      <div className={styles.frame}>
+        <Image alt="frames" src={images.frame2} />
+      </div>
+    </section>
+  );
 }
