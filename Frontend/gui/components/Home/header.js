23c19b68017aa363e5b689d447d5a49480ac01e3
Primal <81525899+Seun2255@users.noreply.github.com>
2022-08-15 15:49:33 +0100

    frontend styling fix

--- Frontend/gui/components/Home/header.js
+++ Frontend/gui/components/Home/header.js
@@ -5,6 +5,7 @@ import styles from "../../styles/Home/header.module.css";
 import images from "../../assets/images/images";
 import ReactPlayer from "react-player";
 import { useState, useEffect } from "react";
+import icons from "../../assets/icons/icons";
 
 export default function Header() {
   const [hydrated, setHydrated] = useState(false);
@@ -16,7 +17,12 @@ export default function Header() {
     <header className={styles.header}>
       <nav className={styles.nav}>
         <div className={styles.logo__container}>
-          <h2 className={styles.logo}>CompareX</h2>
+          <div className={styles.logo__name}>
+            <div className={styles.logo__icon}>
+              <Image src={icons.logo} layout="fill" />
+            </div>
+            <h2 className={styles.logo}>CompareX</h2>
+          </div>
           <ul className={styles.list}>
             <a className={styles.list__item} href="#work">
               How it works?
@@ -41,52 +47,54 @@ export default function Header() {
         </span>
       </nav>
 
-      <main className={styles.main}>
-        <div className={styles.content}>
-          <h4 className={styles.tool}>
-            <span className={styles.crown}>
-              <Image src={images.crown} />
-            </span>{" "}
-            NEW TOOL
-          </h4>
-          <h1 className={styles.easier}>
-            Comparing Excel Sheets
-            <br />
-            Just Got Easier!
-          </h1>
-          <p className={styles.save}>
-            Save your time and build and build productivity with the world-best
-            performing excel
-            <br />
-            file comparison tool
-          </p>
-          <span className={styles.use}>
-            <Link href={"/signup"}>
-              <span className={styles.btn_3}>Use for free</span>
-            </Link>
-            <div className={styles.watch__demo}>
-              <a href="#view" className={styles.btn_4}>
-                Watch Demo
-              </a>
-              <div className={styles.arrow}>
-                <Image src={images.vector} />
+      <div className={styles.main__container}>
+        <main className={styles.main}>
+          <div className={styles.content}>
+            <h4 className={styles.tool}>
+              <span className={styles.crown}>
+                <Image src={images.crown} />
+              </span>{" "}
+              NEW TOOL
+            </h4>
+            <h1 className={styles.easier}>
+              Comparing Excel Sheets
+              <br />
+              Just Got Easier!
+            </h1>
+            <p className={styles.save}>
+              Save your time and build and build productivity with the
+              world-best performing excel
+              <br />
+              file comparison tool
+            </p>
+            <span className={styles.use}>
+              <Link href={"/signup"}>
+                <span className={styles.btn_3}>Use for free</span>
+              </Link>
+              <div className={styles.watch__demo}>
+                <a href="#view" className={styles.btn_4}>
+                  Watch Demo
+                </a>
+                <div className={styles.arrow}>
+                  <Image src={images.vector} />
+                </div>
               </div>
-            </div>
-          </span>
-        </div>
-        {/* End of Background */}
-        {/* Video */}
-        {hydrated && (
-          <ReactPlayer
-            className={styles.video}
-            width="700px"
-            height="400px"
-            id="view"
-            url="https://www.youtube.com/watch?v=Y-Z21liJ1-U"
-            controls
-          />
-        )}
-      </main>
+            </span>
+          </div>
+          {/* End of Background */}
+          {/* Video */}
+          {hydrated && (
+            <ReactPlayer
+              className={styles.video}
+              width="70%"
+              height="400px"
+              id="view"
+              url="https://www.youtube.com/watch?v=Y-Z21liJ1-U"
+              controls
+            />
+          )}
+        </main>
+      </div>
     </header>
   );
 }
